37aaa29e0ea22cc990d0509b3a4cf0c7f89c0b44
Anton <anton260903@gmail.com>
2023-03-27 00:53:51 +0300

    base ViewModel code added

--- PicEdit/ViewModels/Base/ViewModel.cs
+++ PicEdit/ViewModels/Base/ViewModel.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PicEdit.ViewModels.Base
 {
-    internal class ViewModel
+    internal abstract class ViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if(Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
---
+++ PicEdit/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using PicEdit.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicEdit.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+		#region Window title
+		private string _title = "PicEdit";
+
+		/// <summary>
+		/// Window title.
+		/// </summary>
+		public string Title
+		{
+			get => _title;
+			//set
+			//{
+			//	if (Equals(_title, value)) return;
+			//	_title = value;
+			//	OnPropertyChanged();
+
+			//	Set(ref _title, value);
+			//}
+			set => Set(ref _title, value);
+		} 
+		#endregion
+
+	}
+}
