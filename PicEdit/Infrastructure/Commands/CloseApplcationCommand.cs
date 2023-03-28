f0861efccebd44837d30d0bdefabb94a87618d2a
Anton <anton260903@gmail.com>
2023-03-28 17:15:10 +0300

    separate close command class added

---
+++ PicEdit/Infrastructure/Commands/CloseApplcationCommand.cs
@@ -0,0 +1,17 @@
+﻿using PicEdit.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PicEdit.Infrastructure.Commands
+{
+    internal class CloseApplcationCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+    }
+}
