2e9dc820e20b0c22548645c912f5243c78900e8b
Anton <anton260903@gmail.com>
2023-03-28 17:05:24 +0300

    close command in viewModel of main window

--- PicEdit/Infrastructure/Commands/LambdaCommand.cs
+++ PicEdit/Infrastructure/Commands/LambdaCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PicEdit.Infrastructure.Commands.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace PicEdit.Infrastructure.Commands
 {
-    class LambdaCommand
+    internal class LambdaCommand : Command
     {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+        public override bool CanExecute(object? parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object? parameter) => _Execute(parameter);
     }
 }
--- PicEdit/ViewModels/MainWindowViewModel.cs
+++ PicEdit/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,12 @@
-﻿using PicEdit.ViewModels.Base;
+﻿using PicEdit.Infrastructure.Commands;
+using PicEdit.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace PicEdit.ViewModels
 {
@@ -27,8 +30,31 @@ namespace PicEdit.ViewModels
 			//	Set(ref _title, value);
 			//}
 			set => Set(ref _title, value);
-		} 
-		#endregion
+		}
+        #endregion
 
-	}
+        #region Commands
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+
+		private bool OnCloseApplicationCommandExecute(object p) => true;
+
+		private void OnCloseApplicationCommandExecuted(object p)
+		{
+			Application.Current.Shutdown();
+		}
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+			#region Commands
+
+			CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, OnCloseApplicationCommandExecute);
+
+            #endregion
+        }
+    }
 }
