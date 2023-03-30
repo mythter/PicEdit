f8df0b0942b4829a4024c7c82925d0f4a2d8b322
Anton <anton260903@gmail.com>
2023-03-30 14:00:01 +0300

    http request to get raw data

--- Tests/PicEditConsole/Program.cs
+++ Tests/PicEditConsole/Program.cs
@@ -2,9 +2,18 @@
 {
     internal class Program
     {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            HttpClient client = new HttpClient();
+
+            var response = client.GetAsync(data_url).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+
+            Console.ReadLine();
         }
     }
 }
