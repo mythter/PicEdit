e2403de404a14c7ff256d52e119a3af08e69cde9
Anton <anton260903@gmail.com>
2023-05-13 21:50:33 +0300

    crop added

--- PicEdit/ViewModels/MainWindowViewModel.cs
+++ PicEdit/ViewModels/MainWindowViewModel.cs
@@ -17,6 +17,11 @@ using System.Windows.Forms;
 using System.Collections.Specialized;
 using static System.Net.Mime.MediaTypeNames;
 using System.Xml.Linq;
+using System.Collections;
+using System.Windows.Media.Media3D;
+using System.Diagnostics.Contracts;
+using System.Diagnostics;
+using Microsoft.Internal.VisualStudio.PlatformUI;
 
 namespace PicEdit.ViewModels
 {
@@ -68,21 +73,21 @@ namespace PicEdit.ViewModels
 
         #region MainImage Format
         /// <summary>
-        /// Format of a main image.
+        /// Format of the main image.
         /// </summary>
         private ImageFormat _format;
         #endregion
 
         #region MainImage Save Format
         /// <summary>
-        /// Format to save a main image.
+        /// Format to save the main image.
         /// </summary>
         private string _saveFormat = "png";
         #endregion
 
         #region MainImage Path
         /// <summary>
-        /// Path of a main image.
+        /// Path of the main image.
         /// </summary>
         private string _path = "";
         #endregion
@@ -91,7 +96,7 @@ namespace PicEdit.ViewModels
         private bool _isScaleChecked = false;
 
         /// <summary>
-        /// Is scaleX and scaleY do synchronously
+        /// Is scaleX and scaleY do synchronously.
         /// </summary>
         public bool IsScaleChecked
         {
@@ -107,7 +112,7 @@ namespace PicEdit.ViewModels
         private bool _isRotationToolChecked;
 
         /// <summary>
-        /// Is rotation tool checked
+        /// Is rotation tool checked.
         /// </summary>
         public bool IsRotationToolChecked
         {
@@ -140,7 +145,7 @@ namespace PicEdit.ViewModels
         private bool _isScaleToolChecked;
 
         /// <summary>
-        /// Is scale tool checked
+        /// Is scale tool checked.
         /// </summary>
         public bool IsScaleToolChecked
         {
@@ -175,7 +180,7 @@ namespace PicEdit.ViewModels
         private double _scaleXValue = 1;
 
         /// <summary>
-        /// ScaleX value of main image
+        /// ScaleX value of main image.
         /// </summary>
         public double ScaleXValue
         {
@@ -191,7 +196,7 @@ namespace PicEdit.ViewModels
         private double _scaleYValue = 1;
 
         /// <summary>
-        /// ScaleY value of main image
+        /// ScaleY value of main image.
         /// </summary>
         public double ScaleYValue
         {
@@ -207,7 +212,7 @@ namespace PicEdit.ViewModels
         private int _angleValue = 0;
 
         /// <summary>
-        /// Angle value of main image
+        /// Angle value of main image.
         /// </summary>
         public int AngleValue
         {
@@ -223,7 +228,7 @@ namespace PicEdit.ViewModels
         private double _zoomValue = 1;
 
         /// <summary>
-        /// Zoom value for scaling the image
+        /// Zoom value for scaling the image.
         /// </summary>
         public double ZoomValue
         {
@@ -236,7 +241,7 @@ namespace PicEdit.ViewModels
         private bool _isSaveEnabled = false;
 
         /// <summary>
-        /// Property of "Save" and "Save as" buttons in menu.
+        /// Is "Save" and "Save as" buttons enabled menu.
         /// </summary>
         public bool IsSaveEnabled
         {
@@ -252,7 +257,7 @@ namespace PicEdit.ViewModels
         private bool _isRotationEnabled = false;
 
         /// <summary>
-        /// Is Rotation Slider Enabled
+        /// Is Rotation Slider Enabled.
         /// </summary>
         public bool IsRotationEnabled
         {
@@ -268,7 +273,7 @@ namespace PicEdit.ViewModels
         private bool _isScaleEnabled = false;
 
         /// <summary>
-        /// Is Scale Slider Enabled
+        /// Is Scale Slider Enabled.
         /// </summary>
         public bool IsScaleEnabled
         {
@@ -284,7 +289,7 @@ namespace PicEdit.ViewModels
         private int _sliderXValue = 100;
 
         /// <summary>
-        /// Slider X value
+        /// Slider X value.
         /// </summary>
         public int SliderXValue
         {
@@ -303,7 +308,7 @@ namespace PicEdit.ViewModels
         private int _sliderYValue = 100;
 
         /// <summary>
-        /// Slider Y value
+        /// Slider Y value.
         /// </summary>
         public int SliderYValue
         {
@@ -420,11 +425,11 @@ namespace PicEdit.ViewModels
         }
         #endregion
 
-        #region Is Thickness Enabled
+        #region Is Paint Enabled
         private bool _isPaintEnabled = false;
 
         /// <summary>
-        /// Is Rotation Slider Enabled
+        /// Is Paint Enabled.
         /// </summary>
         public bool IsPaintEnabled
         {
@@ -463,7 +468,7 @@ namespace PicEdit.ViewModels
         private int _thicknessValue = 1;
 
         /// <summary>
-        /// Angle value of main image
+        /// Pen Tool Thickness Value.
         /// </summary>
         public int ThicknessValue
         {
@@ -490,16 +495,131 @@ namespace PicEdit.ViewModels
         }
         #endregion
 
-        #region InkCanvas Strokes
+        #region Is InkCanvas Visible
         private bool _isInkCanvasVisible;
 
         /// <summary>
-        /// InkCanvas Stroke Collection.
+        /// Is InkCanvas Visible.
         /// </summary>
         public bool IsInkCanvasVisible
         {
             get => _isInkCanvasVisible;
             set => Set(ref _isInkCanvasVisible, value);
+        }
+        #endregion
+
+        #region Is Crop Enabled
+        private bool _isCropEnabled = false;
+
+        /// <summary>
+        /// Is Crop Enabled.
+        /// </summary>
+        public bool IsCropEnabled
+        {
+            get => _isCropEnabled;
+            set => Set(ref _isCropEnabled, value);
+        }
+        #endregion
+
+        #region Top Crop Value
+        private int _topCropValue = 0;
+
+        /// <summary>
+        /// Top Crop Value.
+        /// </summary>
+        public int TopCropValue
+        {
+            get => _topCropValue;
+            set
+            {
+                Set(ref _topCropValue, value);
+                Image = CropImage(LeftCropValue, TopCropValue, RightCropValue, BottomCropValue);
+            }
+        }
+        #endregion
+
+        #region Bottom Crop Value
+        private int _bottomCropValue = 0;
+
+        /// <summary>
+        /// Bottom Crop Value.
+        /// </summary>
+        public int BottomCropValue
+        {
+            get => _bottomCropValue;
+            set
+            {
+                Set(ref _bottomCropValue, value);
+                Image = CropImage(LeftCropValue, TopCropValue, RightCropValue, BottomCropValue);
+            }
+        }
+        #endregion
+
+        #region Right Crop Value
+        private int _rightCropValue = 0;
+
+        /// <summary>
+        /// Right Crop Value.
+        /// </summary>
+        public int RightCropValue
+        {
+            get => _rightCropValue;
+            set
+            {
+                Set(ref _rightCropValue, value);
+                //Image = CropImage(LeftCropValue, 0, 0, 0);
+                Image = CropImage(LeftCropValue, TopCropValue, RightCropValue, BottomCropValue);
+            }
+        }
+        #endregion
+
+        #region Left Crop Value
+        private int _leftCropValue = 0;
+
+        /// <summary>
+        /// Left Crop Value.
+        /// </summary>
+        public int LeftCropValue
+        {
+            get => _leftCropValue;
+            set
+            {
+                Set(ref _leftCropValue, value);
+                //Image = CropImage(LeftCropValue, 0, 0, 0);
+                Image = CropImage(LeftCropValue, TopCropValue, RightCropValue, BottomCropValue);
+            }
+        }
+        #endregion
+
+        #region Is Crop Tool Checked
+        private bool _isCropToolChecked;
+
+        /// <summary>
+        /// Is crop tool checked
+        /// </summary>
+        public bool IsCropToolChecked
+        {
+            get => _isCropToolChecked;
+            set
+            {
+                Set(ref _isCropToolChecked, value);
+                if (IsCropToolChecked && IsSaveEnabled)
+                    IsCropEnabled = true;
+                else
+                    IsCropEnabled = false;
+
+                if (!IsCropToolChecked && Image != null && (TopCropValue != 0 || BottomCropValue != 0 || LeftCropValue != 0 || RightCropValue != 0))
+                {
+                    int count = obCollection.Count;
+                    if (position != count - 1)
+                        for (int i = count - 1; i > position; i--)
+                            obCollection.RemoveAt(i);
+
+                    obCollection.Add(CropImage(LeftCropValue, TopCropValue, RightCropValue, BottomCropValue));
+                    Image = obCollection[++position];
+                    TopCropValue = BottomCropValue = RightCropValue = LeftCropValue = 0;
+                }
+            }
         }
         #endregion
 
@@ -845,7 +965,18 @@ namespace PicEdit.ViewModels
                     Image = obCollection[++position];
                     AngleValue = 0;
                 }
-                if(IsInkCanvasVisible && p is InkCanvas inkCanv && obStrokeCollection.Count > 1)
+                if (TopCropValue != 0 || BottomCropValue != 0 || LeftCropValue != 0 || RightCropValue != 0)
+                {
+                    int count = obCollection.Count;
+                    if (position != count - 1)
+                        for (int i = count - 1; i > position; i--)
+                            obCollection.RemoveAt(i);
+
+                    obCollection.Add(CropImage(LeftCropValue, TopCropValue, RightCropValue, BottomCropValue));
+                    Image = obCollection[++position];
+                    TopCropValue = BottomCropValue = RightCropValue = LeftCropValue = 0;
+                }
+                if (IsInkCanvasVisible && p is InkCanvas inkCanv && obStrokeCollection.Count > 1)
                 {
                     int count = obCollection.Count;
                     if (position != count - 1)
@@ -862,6 +993,7 @@ namespace PicEdit.ViewModels
                 imageStream = StreamFromBitmapSource(Image);
                 var img = System.Drawing.Image.FromStream(imageStream);
                 img.Save(path, format);
+                System.Windows.MessageBox.Show("Image saved successfully", "Image saved", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
 
@@ -921,22 +1053,173 @@ namespace PicEdit.ViewModels
             //return ConvertBitmapToBitmapSource(bitmap);
         }
 
-        //public static BitmapSource ConvertBitmapToBitmapSource(System.Drawing.Bitmap bitmap)
+        public static byte[] ConvertWriteableBitmapToByteArray(WriteableBitmap wb)
+        {
+            if (wb == null || wb.PixelHeight == 0 || wb.PixelWidth == 0)
+                return null;
+
+            int stride = wb.PixelWidth * wb.Format.BitsPerPixel / 8;
+            int size = stride * wb.PixelHeight;
+
+            byte[] buffer = new byte[size];
+
+            wb.CopyPixels(buffer, stride, 0);
+
+            return buffer;
+        }
+
+        private CroppedBitmap CropImage(int left, int top, int right, int bottom)
+        {
+            left = (int)(obCollection[position].PixelWidth * left / 100f);
+            right = (int)(obCollection[position].PixelWidth * right / 100f);
+            top = (int)(obCollection[position].PixelHeight * top / 100f);
+            bottom = (int)(obCollection[position].PixelHeight * bottom / 100f);
+            return new CroppedBitmap(obCollection[position], new Int32Rect(left, top, obCollection[position].PixelWidth - left - right, obCollection[position].PixelHeight - bottom - top));
+        }
+
+        //private BitmapImage ConvertWriteableBitmapToBitmapImage(WriteableBitmap wbm)
         //{
-        //    var bitmapData = bitmap.LockBits(
-        //        new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
-        //        System.Drawing.Imaging.ImageLockMode.ReadOnly, bitmap.PixelFormat);
-
-        //    var bitmapSource = BitmapSource.Create(
-        //        bitmapData.Width, bitmapData.Height,
-        //        bitmap.HorizontalResolution, bitmap.VerticalResolution,
-        //        PixelFormats.Bgr24, null,
-        //        bitmapData.Scan0, bitmapData.Stride * bitmapData.Height, bitmapData.Stride);
-
-        //    bitmap.UnlockBits(bitmapData);
-
-        //    return bitmapSource;
+        //    BitmapImage bmImage = new BitmapImage();
+        //    using (MemoryStream stream = new MemoryStream())
+        //    {
+        //        PngBitmapEncoder encoder = new PngBitmapEncoder();
+        //        encoder.Frames.Add(BitmapFrame.Create(wbm));
+        //        encoder.Save(stream);
+        //        bmImage.BeginInit();
+        //        bmImage.CacheOption = BitmapCacheOption.OnLoad;
+        //        bmImage.StreamSource = stream;
+        //        bmImage.EndInit();
+        //        bmImage.Freeze();
+        //    }
+        //    return bmImage;
         //}
+
+        //private byte[] ConvertBitmapSourceToByteArray(BitmapSource bitmapSource)
+        //{
+        //    int width = bitmapSource.PixelWidth;
+        //    int height = bitmapSource.PixelHeight;
+        //    int stride = width * ((bitmapSource.Format.BitsPerPixel + 7) / 8);
+
+        //    byte[] bitmapData = new byte[height * stride];
+
+        //    bitmapSource.CopyPixels(bitmapData, stride, 0);
+        //    return bitmapData;
+        //}
+
+        //private WriteableBitmap ChangeBrightness(WriteableBitmap source, byte change_value)
+        //{
+        //    WriteableBitmap dest = new WriteableBitmap(source);
+
+        //    byte[] color = new byte[4];
+
+        //    using (Stream s = new MemoryStream(ConvertWriteableBitmapToByteArray(source)))
+        //    {
+        //        using (Stream d = new MemoryStream(ConvertWriteableBitmapToByteArray(dest)))
+        //        {
+        //            // read the pixel color
+        //            while (s.Read(color, 0, 4) > 0)
+        //            {
+        //                // color[0] = b
+        //                // color[1] = g 
+        //                // color[2] = r
+        //                // color[3] = a
+
+        //                // do the adding algo per byte (skip the alpha)
+        //                for (int i = 0; i < 4; i++)
+        //                {
+        //                    if (color[i] + change_value > 255) color[i] = 255; else color[i] = (byte)(color[i] + change_value);
+        //                }
+
+        //                // write the new pixel color
+        //                d.Write(color, 0, 4);
+        //            }
+        //        }
+        //    }
+        //    //using (FileStream stream = new FileStream("./test.png", FileMode.Create))
+        //    //{
+        //    //    PngBitmapEncoder encoder = new PngBitmapEncoder();
+        //    //    encoder.Frames.Add(BitmapFrame.Create(dest.Clone()));
+        //    //    encoder.Save(stream);
+        //    //}
+        //    // return the new bitmap
+        //    return dest;
+        //}
+
+        //public Bitmap AdjustBrightnessContrast(System.Drawing.Image image, int contrastValue, int brightnessValue)
+        //{
+        //    float brightness = -(brightnessValue / 100.0f);
+        //    float contrast = contrastValue / 100.0f;
+        //    var bitmap = new Bitmap(image.Width, image.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+        //    using (var g = Graphics.FromImage(bitmap))
+        //        using (var attributes = new ImageAttributes())
+        //        {
+        //            float[][] matrix = {
+        //                new float[] { contrast, 0, 0, 0, 0},
+        //                new float[] {0, contrast, 0, 0, 0},
+        //                new float[] {0, 0, contrast, 0, 0},
+        //                new float[] {0, 0, 0, 1, 0},
+        //                new float[] {brightness, brightness, brightness, 1, 1}
+        //        };
+
+        //        ColorMatrix colorMatrix = new ColorMatrix(matrix);
+        //        attributes.SetColorMatrix(colorMatrix);
+        //        g.DrawImage(image, new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+        //            0, 0, bitmap.Width, bitmap.Height, GraphicsUnit.Pixel, attributes);
+        //        return bitmap;
+        //    }
+        //}
+
+        //private System.Drawing.Bitmap BitmapFromSource(BitmapSource bitmapsource)
+        //{
+        //    System.Drawing.Bitmap bitmap;
+        //    using (MemoryStream outStream = new MemoryStream())
+        //    {
+        //        PngBitmapEncoder enc = new PngBitmapEncoder();
+        //        enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+        //        enc.Save(outStream);
+        //        bitmap = new System.Drawing.Bitmap(outStream);
+        //    }
+        //    return bitmap;
+        //}
+
+        //private static BitmapImage BitmapToSource(Bitmap src)
+        //{
+        //    System.IO.MemoryStream ms = new System.IO.MemoryStream();
+        //    src.Save(ms, ImageFormat.Jpeg);
+
+        //    BitmapImage image = new BitmapImage();
+        //    image.BeginInit();
+        //    ms.Seek(0, System.IO.SeekOrigin.Begin);
+        //    image.StreamSource = ms;
+        //    image.EndInit();
+        //    return image;
+        //}
+
+        //private static BitmapSource ConvertBitmapToBitmapSource(System.Drawing.Bitmap bitmap)
+        //{
+        //    //var bitmapData = bitmap.LockBits(
+        //    //    new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+        //    //    System.Drawing.Imaging.ImageLockMode.ReadOnly, bitmap.PixelFormat);
+
+        //    //var bitmapSource = BitmapSource.Create(
+        //    //    bitmapData.Width, bitmapData.Height,
+        //    //    bitmap.HorizontalResolution, bitmap.VerticalResolution,
+        //    //    PixelFormats.Bgr24, null,
+        //    //    bitmapData.Scan0, bitmapData.Stride * bitmapData.Height, bitmapData.Stride);
+
+        //    //bitmap.UnlockBits(bitmapData);
+
+        //    //return bitmapSource;
+
+        //    return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+        //      bitmap.GetHbitmap(),
+        //      IntPtr.Zero,
+        //      Int32Rect.Empty,
+        //      BitmapSizeOptions.FromEmptyOptions());
+        //}
+
+
 
         //static byte[] GetBytesFromBitmapSource(BitmapSource bmp)
         //{
